cb32a9d6e7d20d5710977f14129548708cc9ab09
Ghulammustafa Kakar <mustafakakar01@yahoo.com>
2024-12-07 22:26:06 +0100

    feat: update registration page to use Material Design
    Refactored the registration page to incorporate Material Design components, including buttons and inputs.

--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -1,11 +1,22 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
+import { FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-register',
-  imports: [],
+  imports: [MatButtonModule, MatFormFieldModule, MatInputModule, ReactiveFormsModule, MatIconModule],
   templateUrl: './register.component.html',
-  styleUrl: './register.component.scss'
+  styleUrl: './register.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class RegisterComponent {
-
+  emailFormControl = new FormControl('', [Validators.required, Validators.email]);
+  hide = signal(true);
+  clickEvent(event: MouseEvent) {
+    this.hide.set(!this.hide());
+    event.stopPropagation();
+  }
 }
